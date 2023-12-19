cd6e0469a93bb6faca1ce8168a5f4cb45337151e
alice-ibba5 <135819129+alice-ibba5@users.noreply.github.com>
2023-12-20 00:35:41 +0100

    up

--- src/views/registration/Registration.jsx
+++ src/views/registration/Registration.jsx
@@ -77,7 +77,7 @@ const Registration = ({ blogPosts, setblogPosts }) => {
                                 'Content-Type': 'application/json',
                             },
                             body: JSON.stringify({
-                                authorEmail,
+                                email,
                             }),
                         });
 
